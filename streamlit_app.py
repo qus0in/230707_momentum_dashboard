dc5875a7a3d7da1ff86908d56659fa45acb08beb
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-07 10:40:01 +0900

    use_container_width=1

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,9 @@ with col1:
     table_name = 'recent_momentum_score'
     data = get_table_from_supabase(table_name)
     df = pd.DataFrame(data)
-    st.dataframe(df, hide_index=1)
+    st.dataframe(df, hide_index=1, use_container_width=1)
 
 with col2:
     df2 = df[df.score >= df.score.iloc[3]].query('score > 0')
     df2['Unit'] = (0.01 / df2.aatr) * st.session_state.total
-    st.dataframe(df2.iloc[:, [0, 1, 4]], hide_index=1)
+    st.dataframe(df2.iloc[:, [0, 1, 4]], hide_index=1, use_container_width=1)
