bf6b259fb2c12345ffcbdd59366a192d363958e1
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-07 10:39:06 +0900

    Unit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ option = dict(
     label = "자산총액",
     min_value = 0,
     step=1,
+    key='total'
 )
 st.number_input(**option)
 
@@ -34,4 +35,5 @@ with col1:
 
 with col2:
     df2 = df[df.score >= df.score.iloc[3]].query('score > 0')
-    st.dataframe(df2, hide_index=1)
+    df2['Unit'] = (0.01 / df2.aatr) * st.session_state.total
+    st.dataframe(df2.iloc[:, [0, 1, 4]], hide_index=1)
