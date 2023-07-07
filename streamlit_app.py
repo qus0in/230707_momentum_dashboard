9fafd934223621dc64f7777c89d7d36e12f2ddc0
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-07 10:19:31 +0900

    params

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import streamlit as st
 SUPABASE_URL = os.environ.get('SUPABASE_URL')
 SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
 
-def get_table_from_supabase(table_name):
+def get_table_from_supabase(table_name, params):
     url = f"{SUPABASE_URL}/rest/v1/{table_name}"
-    params = dict(select="*")
+    # params = dict(select="*")
     headers = dict(
         apikey=SUPABASE_KEY,
         Authorization=f"Bearer {SUPABASE_KEY}")
@@ -16,4 +16,5 @@ def get_table_from_supabase(table_name):
     data = response.json()
     return data
 
-st.write(get_table_from_supabase('momentum_score'))
+params = dict(group='created_at', select='created_at')
+st.write(get_table_from_supabase('momentum_score', params))
