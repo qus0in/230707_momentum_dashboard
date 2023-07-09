e3d8a1a5806b9209538c07c08d36216e7c6e99ff
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-09 18:05:49 +0900

    st.session_state.total

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ with col2:
     df2 = df[df.score >= df.score.iloc[3]].query('score > 0')
     df2['Unit'] = (((0.01 / df2.aatr) / 4 * st.session_state.total // 10000).apply(math.floor) * 10000)
     st.dataframe(df2.iloc[:, [0, 4]], hide_index=1, use_container_width=1)
+    st.write(df2.Unit.sum() / st.session_state.total)
 
 with st.expander("Cluster Groups"):
     table_name = 'recent_cluster_groups'
@@ -50,7 +51,7 @@ with st.expander("Cluster Groups"):
     df3 = pd.DataFrame(data).apply(lambda x: x.str.split(', '))
     st.dataframe(df3, hide_index=1, use_container_width=1,
     column_config={
-        "cluster_groups": st.column_config.ListColumn("그룹별 종목", width="small")
+        "cluster_groups": st.column_config.ListColumn("그룹별 종목")
     })
 
 st.image('./welcome.png', use_column_width=1)
