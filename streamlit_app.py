549b7157c50761c2c1ba9d7cef7da5d71242d3df
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-08-03 16:07:34 +0900

    min(x, 1)

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ with col1:
 
 with col2:
     df2 = df[df.score >= df.score.iloc[4]].query('score > 0')
-    df2['unit'] = ((((0.01 / df2.aatr) / 4 * st.session_state.total).apply(lambda x: min([x, st.session_state.total // 4])) // 1000000).apply(math.floor) * 1000000)
+    df2['unit'] = (((0.01 / df2.aatr).apply(lambda x: min(x, 1)) / 4 * st.session_state.total // 1000000).apply(math.floor) * 1000000)
     st.dataframe(df2.loc[:, ['symbol', 'unit']], hide_index=True, use_container_width=True)
     st.metric("위험 조정 후 주식 비중", f"{math.floor(df2.unit.sum() / st.session_state.total * 10000) / 100}%")
 
